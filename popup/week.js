55fe686b9521b75264e1ab1187bbcadc3b178b4a
Om3r <wb_omer@hotmail.com>
2022-06-06 21:17:57 +0300

    fix week problem

--- popup/week.js
+++ popup/week.js
@@ -3,8 +3,7 @@ startDate = new Date(currentDate.getFullYear(), 0, 1);
 var days = Math.floor((currentDate - startDate) /
     (24 * 60 * 60 * 1000));
       
-var weekNumber = Math.ceil(
-    (currentDate.getDay() + 1 + days) / 7);
+var weekNumber = Math.ceil(days / 7);
 
 document.getElementById('currentDate').textContent = currentDate.toISOString().split('T')[0];
 document.getElementById('week').textContent = weekNumber;
