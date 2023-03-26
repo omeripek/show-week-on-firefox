b0ff601303b097bcedc8a8e70a80473698f0271c
omeripek <50427567+omeripek@users.noreply.github.com>
2023-03-26 17:51:59 +0300

    Update week.js

--- popup/week.js
+++ popup/week.js
@@ -1,9 +1,14 @@
-currentDate = new Date();
-startDate = new Date(currentDate.getFullYear(), 0, 1);
-var days = Math.floor((currentDate - startDate) /
-    (24 * 60 * 60 * 1000));
-      
-var weekNumber = Math.ceil(( currentDate.getDay() + 1 + days) / 7);
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentDay = (currentDate.getDay() + 6) % 7;
+const startOfYear = new Date(currentYear, 0, 1);
+const daysSinceStart = Math.floor((currentDate - startOfYear) / (24 * 60 * 60 * 1000));
+let weekNumber = Math.floor((daysSinceStart + (7 - currentDay) + 1) / 7);
+
+const firstDayOfYear = startOfYear.getDay();
+if (firstDayOfYear === 4 || firstDayOfYear === 5 || firstDayOfYear === 6) {
+    weekNumber++;
+}
 
 document.getElementById('currentDate').textContent = currentDate.toISOString().split('T')[0];
 document.getElementById('week').textContent = weekNumber;
